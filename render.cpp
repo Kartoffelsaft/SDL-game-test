5a5943f3799d0f1225c35072a0afb4ae0fd91c27
Ben Findley <ben.g.findley@gmail.com>
2018-08-23 11:16:44 -0700

    renders a stretching line

--- render.cpp
+++ render.cpp
@@ -9,12 +9,26 @@ using namespace RenderData;
 
 void render()
 {
+  static int startx{0};
+
+  if(SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF) != 0)
+  {
+    std::cout << SDL_GetError() << std::endl;
+  }
+
+  SDL_RenderClear(renderer);
+
   if(SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF) != 0)
   {
     std::cout << SDL_GetError() << std::endl;
   }
-  if(SDL_RenderDrawLine(renderer, 30, 30, 60, 60))
+  if(SDL_RenderDrawLine(renderer, startx, 30, 60, 60) != 0)
   {
     std::cout << SDL_GetError() << std::endl;
   }
+  startx++;
+
+  SDL_Delay(100);
+
+  SDL_RenderPresent(renderer);
 }
