7506472499df31d0230315a1dbd44459877a15f7
Benjamin Findley <ben.g.findley@gmail.com>
2018-08-28 09:09:23 -0700

    minor rotation changes

--- geometry.cpp
+++ geometry.cpp
@@ -36,8 +36,11 @@ namespace Geometry
     // if(this->rotation.z < -sinY)
     // {this->rotation.z = -sinY;}
 
-    this->rotation.z += -direction.z;
+    this->rotation.z += -direction.z; //only use shortly before reseting the camera rotation; it's kind of broken.
     this->rotation.x += quickCosine(this->rotation.z) * -direction.x + quickSine(-this->rotation.z) * direction.y;
     this->rotation.y += quickCosine(this->rotation.z) * -direction.y + quickSine(this->rotation.z) * direction.x;
+
+    this->rotation.y = fmodf(this->rotation.y, 360);
+    this->rotation.x = fmodf(this->rotation.x, 360);
   }
 }
