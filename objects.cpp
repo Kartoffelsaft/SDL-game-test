9f8715c6cd79bef00b23a22c1a5dc54060bebe12
Benjamin Findley <ben.g.findley@gmail.com>
2018-08-29 16:05:29 -0700

    can read .obj file faces

--- objects.cpp
+++ objects.cpp
@@ -17,34 +17,55 @@ mesh generateMesh(const char* objFile, point location)
 
   while(getline(file, content))
   {
-    content.push_back(' ');
+    content.push_back('\n');
 
     if(content[0] == 'v')
     {
       newMesh.vertecies.push_back({0, 0, 0});
 
+      int dimension{0};
+      std::string newNumber{""};
+      for(int i{1}; i < content.size(); i++)
       {
-        int dimension{0};
-        std::string newNumber{""};
-        for(int i{1}; i < content.size(); i++)
+        if(content.at(i) == ' ' || content.at(i) == '\n')
         {
-          if(content.at(i) == ' ')
-          {
-            if(dimension == 1)
-            {newMesh.vertecies.back().x = std::stof(newNumber);}
-            if(dimension == 2)
-            {newMesh.vertecies.back().y = std::stof(newNumber);}
-            if(dimension == 3)
-            {newMesh.vertecies.back().z = std::stof(newNumber);}
-            if(dimension > 3)
-            {break;}
+          if(dimension == 1)
+          {newMesh.vertecies.back().x = std::stof(newNumber);}
+          if(dimension == 2)
+          {newMesh.vertecies.back().y = std::stof(newNumber);}
+          if(dimension == 3)
+          {newMesh.vertecies.back().z = std::stof(newNumber);}
+          if(dimension > 3)
+          {break;}
 
-            newNumber = "";
-            dimension++;
-          }
-          else
-          {newNumber.push_back(content.at(i));}
+          newNumber = "";
+          dimension++;
         }
+        else
+        {newNumber.push_back(content.at(i));}
+      }
+    }
+
+    if(content[0] == 'f')
+    {
+      newMesh.polys.push_back({});
+
+      std::string newNumber{""};
+
+      for(int i{2}; i < content.size(); i++)
+      {
+        if(content.at(i) == ' ')
+        {
+          newMesh.polys.back().vertecies.push_back(&newMesh.vertecies.at(std::stoi(newNumber) - 1));
+          newNumber = "";
+        }
+        else if(content.at(i) == '\n')
+        {
+          newMesh.polys.back().vertecies.push_back(&newMesh.vertecies.at(std::stoi(newNumber) - 1));
+          break;
+        }
+        else
+        {newNumber.push_back(content.at(i));}
       }
     }
   }
